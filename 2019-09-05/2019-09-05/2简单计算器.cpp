8b932014b407924737273b21e5b607e6d4d15672
陈扬cyX <chenyangcyx@outlook.com>
2019-09-05 20:52:44 +0800

    栈：简单计算器 完成 在原先的基础上，添加了计算器对于小数的支持，并增加了左右括号的匹配功能，经测试可以正常使用

--- 2019-09-05/2019-09-05/2简单计算器.cpp
+++ 2019-09-05/2019-09-05/2简单计算器.cpp
@@ -6,12 +6,14 @@ using namespace std;
 char str[202];
 stack<int> op;
 stack<double> nums;
-int op_priority[][5] = {
-	1,0,0,0,0,
-	1,0,0,0,0,
-	1,0,0,0,0,
-	1,1,1,0,0,
-	1,1,1,0,0
+int op_priority[][7] = {
+	1,0,0,0,0,0,0,
+	1,0,0,0,0,1,0,
+	1,0,0,0,0,1,0,
+	1,1,1,0,0,1,0,
+	1,1,1,0,0,1,0,
+	1,1,1,1,1,1,0,
+	1,0,0,0,0,1,0
 };
 //数字对应的符号：
 //0 - 起始/结束标识符号
@@ -19,6 +21,8 @@ int op_priority[][5] = {
 //2 - -
 //3 - *
 //4 - /
+//5 - (
+//6 - )
 
 void GetStrInfo(bool& if_op, double& num, int& i)
 {
@@ -36,12 +40,27 @@ void GetStrInfo(bool& if_op, double& num, int& i)
 	}
 	if (str[i] >= '0' && str[i] <= '9')
 	{
+		bool if_xiaoshu = false;
+		double xiaoshu_mul = 1.0;
 		if_op = false;
 		num = 0;
 		while (str[i] != ' ' && str[i] != '\0')
 		{
-			num *= 10.0;
-			num += (double)str[i++] - '0';
+			if (if_xiaoshu == false && str[i] == '.')
+			{
+				if_xiaoshu = true;
+				i++;
+			}
+			if (if_xiaoshu == false)
+			{
+				num *= 10.0;
+				num += (double)str[i++] - '0';
+			}
+			else
+			{
+				xiaoshu_mul *= 0.1;
+				num += ((double)str[i++] - '0') * xiaoshu_mul;
+			}
 		}
 		if(str[i]==' ')
 			i++;
@@ -64,10 +83,19 @@ void GetStrInfo(bool& if_op, double& num, int& i)
 		case '/':
 			num = 4;
 			break;
+		case '(':
+			num = 5;
+			break;
+		case ')':
+			num = 6;
+			break;
 		default:
 			break;
 		}
-		i += 2;
+		if (str[i + 1] == ' ')
+			i += 2;
+		else
+			i++;
 		return;
 	}
 }
@@ -88,7 +116,6 @@ int main()
 		while (true)
 		{
 			GetStrInfo(if_op, num, str_i);
-			printf("if_op=%d,num=%f,str_i=%d,op empty=%d\n", if_op, num, str_i, op.empty());
 			if (if_op == false)
 				nums.push(num);
 			else
@@ -107,16 +134,16 @@ int main()
 						nums.pop();
 						switch (temp_op)
 						{
-						case '1':
+						case 1:
 							nums.push(num_1 + num_2);
 							break;
-						case '2':
+						case 2:
 							nums.push(num_1 - num_2);
 							break;
-						case '3':
+						case 3:
 							nums.push(num_1 * num_2);
 							break;
-						case '4':
+						case 4:
 							nums.push(num_1 / num_2);
 							break;
 						default:
@@ -124,6 +151,12 @@ int main()
 						}
 					}
 					op.push(num);
+					int temp_op = op.top();
+					op.pop();
+					if (temp_op == 6 && op.top() == 5)
+						op.pop();
+					else
+						op.push(temp_op);
 				}
 			}
 			if (op.size() == 2 && op.top() == 0)
