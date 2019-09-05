c6972e96b1e3a5e9af5f4d6d1e03e0ad3d6e9f10
陈扬cyX <chenyangcyx@outlook.com>
2019-09-06 00:41:17 +0800

    学习：队列的使用 queue和priority_queue的不同使用方法

---
+++ 2019-09-06/2019-09-06/1队列的使用.cpp
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <queue>
+using namespace std;
+
+void test1()
+{
+	queue<int> qu;
+	for (int i = 0; i < 10; i++)
+		qu.push(i);
+	while (!qu.empty())
+	{
+		printf("%d\n", qu.front());
+		qu.pop();
+	}
+}
+
+struct Node {
+	int x;
+	int y;
+	Node(int xx,int yy):x(xx),y(yy){}
+	//bool operator < (const Node &other) const {
+	//	if (x == other.x)
+	//	{
+	//		return y < other.y;
+	//	}
+	//	else
+	//		return x < other.x;
+	//}
+};
+
+struct cmp
+{
+	bool operator ()(Node& n1, Node& n2)
+	{
+		if (n1.x == n2.x)
+			return n1.y < n2.y;
+		else
+			return n1.x < n2.x;
+	}
+};
+
+void test2()
+{
+	priority_queue<Node,vector<Node>,cmp> qu;
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			qu.push(Node(i, j));
+	while (!qu.empty())
+	{
+		printf("x=%d,y=%d\n", qu.top().x,qu.top().y);
+		qu.pop();
+	}
+}
+
+int main()
+{
+	test1();
+	test2();
+	return 0;
+}
