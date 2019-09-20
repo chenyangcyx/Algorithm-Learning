798c75bdc3dd5f8588235dcba8901b675618c55e
陈扬cyX <chenyangcyx@outlook.com>
2019-09-20 22:15:17 +0800

    学习：最小生成树Prime算法 时间： 2019年9月20日22:15:15

--- 2019-09-20/2019-09-20/3最小生成树Kruskal.cpp
+++ 2019-09-20/2019-09-20/3最小生成树Kruskal.cpp
@@ -1,55 +1,55 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <algorithm>
-using namespace std;
-#define max_n 10000
-
-struct Road
-{
-	int a, b;
-	int length;
-
-	bool operator <(const Road& r) const
-	{
-		return length < r.length;
-	}
-}road[max_n];
-
-int Tree[max_n];
-
-int FindRoot(int x)
-{
-	if (Tree[x] == -1) return x;
-	int tmp = FindRoot(Tree[x]);
-	Tree[x] = tmp;
-	return tmp;
-}
-
-int main()
-{
-	int n;
-	while (scanf("%d", &n) != EOF && n != 0)
-	{
-		memset(Tree, -1, sizeof(Tree));
-		for (int i = 1; i <= n * (n - 1) / 2; i++)
-			scanf("%d %d %d", &road[i].a, &road[i].b, &road[i].length);
-
-		sort(road + 1, road + 1 + n * (n - 1) / 2);
-		int ans = 0;
-		for (int i = 1; i <= n * (n - 1) / 2; i++)
-		{
-			//写法二
-			int a = road[i].a;
-			int b = road[i].b;
-			a = FindRoot(a);
-			b = FindRoot(b);
-			if (a != b)
-			{
-				Tree[a] = b;
-				ans += road[i].length;
-			}
-		}
-		printf("%d\n", ans);
-	}
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <algorithm>
+//using namespace std;
+//#define max_n 10000
+//
+//struct Road
+//{
+//	int a, b;
+//	int length;
+//
+//	bool operator <(const Road& r) const
+//	{
+//		return length < r.length;
+//	}
+//}road[max_n];
+//
+//int Tree[max_n];
+//
+//int FindRoot(int x)
+//{
+//	if (Tree[x] == -1) return x;
+//	int tmp = FindRoot(Tree[x]);
+//	Tree[x] = tmp;
+//	return tmp;
+//}
+//
+//int main()
+//{
+//	int n;
+//	while (scanf("%d", &n) != EOF && n != 0)
+//	{
+//		memset(Tree, -1, sizeof(Tree));
+//		for (int i = 1; i <= n * (n - 1) / 2; i++)
+//			scanf("%d %d %d", &road[i].a, &road[i].b, &road[i].length);
+//
+//		sort(road + 1, road + 1 + n * (n - 1) / 2);
+//		int ans = 0;
+//		for (int i = 1; i <= n * (n - 1) / 2; i++)
+//		{
+//			//写法二
+//			int a = road[i].a;
+//			int b = road[i].b;
+//			a = FindRoot(a);
+//			b = FindRoot(b);
+//			if (a != b)
+//			{
+//				Tree[a] = b;
+//				ans += road[i].length;
+//			}
+//		}
+//		printf("%d\n", ans);
+//	}
+//	return 0;
+//}
--- 2019-09-20/2019-09-20/4最小生成树Prime.cpp
+++ 2019-09-20/2019-09-20/4最小生成树Prime.cpp
@@ -1,6 +1,56 @@
-//#include <stdio.h>
-//
-//int main()
-//{
-//	return 0;
-//}
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <memory.h>
+#include <limits.h>
+#define max_n 1000
+#define INF -1
+
+int graph[max_n][max_n];
+bool visit[max_n];
+int dis[max_n];
+
+int main()
+{
+	int n;
+	while (scanf("%d", &n) != EOF && n != 0)
+	{
+		memset(graph, INF, sizeof(graph));
+		memset(visit, false, sizeof(visit));
+		memset(dis, INF, sizeof(dis));
+		int a, b, c;
+		for (int i = 1; i <= n * (n - 1) / 2; i++)
+		{
+			scanf("%d %d %d", &a, &b, &c);
+			graph[a][b] = graph[b][a] = c;
+		}
+		int ans = 0;
+		//初始化距离数组
+		for (int i = 1; i <= n; i++)
+			dis[i] = graph[1][i];
+		//开始遍历每一个点
+		visit[1] = true;
+		for (int i = 1; i < n; i++)
+		{
+			//找到最小距离的点
+			int min_length = INT_MAX;
+			int min = -1;
+			for (int j = 1; j <= n; j++)
+			{
+				if (!visit[j] && dis[j] < min_length)
+				{
+					min_length = dis[j];
+					min = j;
+				}
+			}
+			//更新标志，将该点加入集合
+			visit[min] = true;
+			ans += min_length;
+			//更新距离dis矩阵
+			for (int j = 1; j <= n; j++)
+				if (!visit[j] && dis[j] > graph[min][j])
+					dis[j] = graph[min][j];
+		}
+		printf("%d\n", ans);
+	}
+	return 0;
+}
