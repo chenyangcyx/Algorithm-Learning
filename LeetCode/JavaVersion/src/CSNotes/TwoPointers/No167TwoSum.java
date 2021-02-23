1b394bd8d6c13ff30613d88e748d5b5697cd72de
chenyangcyx <chenyangcyx@outlook.com>
2021-02-24 01:23:20 +0800

    167. 两数之和 II - 输入有序数组

---
+++ LeetCode/JavaVersion/src/CSNotes/TwoPointers/No167TwoSum.java
@@ -0,0 +1,21 @@
+package CSNotes.TwoPointers;
+
+public class No167TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers == null) return null;
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target){
+                return new int[]{i + 1, j + 1};
+            }
+            else if (sum < target){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return null;
+    }
+}
--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,5 +1,14 @@
+import CSNotes.TwoPointers.No167TwoSum;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Cs Notes
+        // Two Pointers
+        // No 167
+        int[] numbers=new int[]{2,7,11,15};
+        int target=9;
+        System.out.println(Arrays.toString(new No167TwoSum().twoSum(numbers, target)));
     }
 }
