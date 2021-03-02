3c63e46eece08eff9c890a1c1bfd50a6390c18b6
chenyangcyx <chenyangcyx@outlook.com>
2021-03-02 20:03:43 +0800

    95. 不同的二叉搜索树 II

---
+++ LeetCode/JavaVersion/src/CSNotes/DivideAndConquer/No95UniqueBinarySearchTreesII.java
@@ -0,0 +1,50 @@
+package CSNotes.DivideAndConquer;
+import java.util.*;
+
+public class No95UniqueBinarySearchTreesII {
+    //    Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return getSubTree(1, n);
+    }
+
+    private List<TreeNode> getSubTree(int start, int end) {
+        List<TreeNode> tree = new ArrayList<>();
+        if (start > end) {
+            tree.add(null);
+            return tree;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> ltree = getSubTree(start, i - 1);
+            List<TreeNode> rtree = getSubTree(i + 1, end);
+            for (TreeNode lnode : ltree) {
+                for (TreeNode rnode : rtree) {
+                    TreeNode node = new TreeNode(i);
+                    node.left = lnode;
+                    node.right = rnode;
+                    tree.add(node);
+                }
+            }
+        }
+        return tree;
+    }
+}
