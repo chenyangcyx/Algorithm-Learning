129491ac12fe0b72526f3a8f95847e23d1ccfb2f
chenyangcyx <chenyangcyx@outlook.com>
2021-03-04 16:55:31 +0800

    24. 两两交换链表中的节点

---
+++ LeetCode/JavaVersion/src/CSNotes/LinkedList/No24SwapNodesinPairs.java
@@ -0,0 +1,49 @@
+package CSNotes.LinkedList;
+
+public class No24SwapNodesinPairs {
+    //    Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // CS-Note参考代码
+    public ListNode swapPairs1(ListNode head) {
+        ListNode node = new ListNode(-1);
+        node.next = head;
+        ListNode pre = node;
+        while (pre.next != null && pre.next.next != null) {
+            ListNode l1 = pre.next, l2 = pre.next.next;
+            ListNode next = l2.next;
+            l1.next = next;
+            l2.next = l1;
+            pre.next = l2;
+
+            pre = l1;
+        }
+        return node.next;
+    }
+
+    // LeetCode递归写法
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+}
