0e8501b09d2a49451dde54a2ae1c460f8259a206
chenyangcyx <chenyangcyx@outlook.com>
2021-03-08 17:20:54 +0800

    高级算法，No 1-14

--- 高级算法导论/AlgorithmClass/src/Contest1/No1_14.java
+++ 高级算法导论/AlgorithmClass/src/Contest1/No1_14.java
@@ -1,8 +1,31 @@
+package Contest1;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class No1_14 {
+    // 写法1
+    public void No1_14() {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            sc.nextLine();
+            String _str = sc.nextLine();
+            String[] split = _str.split(" ");
+            int[] nums = new int[split.length];
+            for (int i = 0; i < split.length; i++) nums[i] = Integer.parseInt(split[i]);
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length; i++) {
+                if (i == 0) System.out.print(nums[i]);
+                else System.out.print(" " + nums[i]);
+            }
+            System.out.println();
+        }
+    }
+
+    // 写法2
+    public void No1_14_2() {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         while (T-- > 0) {
