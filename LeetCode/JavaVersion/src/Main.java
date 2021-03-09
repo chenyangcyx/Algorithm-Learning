da8eb708b6d347e7495f20fd862ff4d2e658c858
chenyangcyx <chenyangcyx@outlook.com>
2021-03-10 00:00:39 +0800

    LeetCode1047. 删除字符串中的所有相邻重复项

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -7,90 +7,11 @@ import CSNotes.Greedy.No605CanPlaceFlowers;
 import CSNotes.Greedy.No665NondecreasingArray;
 import CSNotes.LinkedList.No19RemoveNthNodeFromEndofList;
 import CSNotes.Sort.No451SortCharactersByFrequency;
+import QuestionBank.No1047removeDuplicates;
 
 public class Main {
     public static void main(String[] args) {
-        // Cs Notes
-        // Two Pointers
-        // No 167
-//        int[] numbers=new int[]{2,7,11,15};
-//        int target=9;
-//        System.out.println(Arrays.toString(new No167TwoSum().twoSum(numbers, target)));
-
-        // No633
-
-        // No 345
-//        String test_str = "leetcode";
-//        System.out.println(new No345ReverseVowelsofaString().reverseVowels(test_str));
-
-        // No 680
-
-        // No 88
-//        int []nums1=new int[]{1,2,3,0,0,0};
-//        int []nums2=new int[]{2,5,6};
-//        int m=3;
-//        int n=3;
-//        int []nums1=new int[]{2,0};
-//        int []nums2=new int[]{1};
-//        int m=1;
-//        int n=1;
-//        int []nums1=new int[]{4,5,6,0,0,0};
-//        int []nums2=new int[]{1,2,3};
-//        int m=3;
-//        int n=3;
-//        new No88MergeSortedArray().merge2(nums1,m,nums2,n);
-//        System.out.println(Arrays.toString(nums1));
-
-        // 快速排序
-//        int []nums=new int[]{100,90,80,70,60,50,40};
-//        new QuickSort().quickSort(nums,0, nums.length-1);
-//        System.out.println(Arrays.toString(nums));
-
-//        // 堆排序
-//        int []nums=new int[]{10,60,22,85,50,20,40,58};
-//        new HeapSort().heapSort(nums);
-//        System.out.println(Arrays.toString(nums));
-
-        // 桶排序
-//        int []nums=new int[]{10,60,22,85,50,20,40,58};
-//        new BucketSort().bucketSort(nums);
-//        System.out.println(Arrays.toString(nums));
-
-        // No 215
-//        int []nums=new int[]{10,60,22,85,50,20,40,58};
-//        System.out.println(new No215KthLargestElementinanArray().findKthLargest3(nums,2));
-
-        // No 451
-//        String str = "cccaaa";
-//        System.out.println(new No451SortCharactersByFrequency().frequencySort1(str));
-
-//        // No605
-//        int[] flowers=new int[]{0,0,1,0,0};
-//        System.out.println(new No605CanPlaceFlowers().canPlaceFlowers1(flowers,3));
-
-//        // No 665
-//        int []nums=new int[]{4,2,3,4};
-//        System.out.println(new No665NondecreasingArray().checkPossibility(nums));
-
-//        // No 53
-//        int []nums=new int[]{-2,1,-3,4,-1,2,1,-5,4};
-//        System.out.println(new No53MaximumSubarray().maxSubArray(nums));
-
-//        // 二分查找
-//        int []nums=new int[]{1,2,3,4,5,6,7,8,9,10};
-//        System.out.println(new BinarySearch().binarySearch(nums,7));
-
-//        // No 744
-//        char []chars=new char[]{'c', 'f', 'j'};
-//        System.out.println(new No744FindSmallestLetterGreaterThanTarget().nextGreatestLetter1(chars,'j'));
-
-//        // No 153
-//        int []nums1=new int[]{4,5,6,7,0,1,2};
-//        int []nums2=new int[]{3,1,2};
-//        System.out.println(new No153FindMinimuminRotatedSortedArray().findMin1(nums2));
-
-//        // No 241
-//        String str="2*3-4*5";
-//        System.out.println(new No241DifferentWaystoAddParentheses().diffWaysToCompute1(str));
+        String str="abbaca";
+        System.out.println(new No1047removeDuplicates().removeDuplicates(str));
     }
 }
---
+++ LeetCode/JavaVersion/src/QuestionBank/No1047removeDuplicates.java
@@ -0,0 +1,20 @@
+package QuestionBank;
+import java.util.LinkedList;
+
+public class No1047removeDuplicates {
+    public String removeDuplicates(String S) {
+        char[] array = S.toCharArray();
+        LinkedList<Character> stack = new LinkedList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (stack.size() == 0) stack.addLast(array[i]);
+            else {
+                char pre = stack.peekLast();
+                if (array[i] == pre) stack.pollLast();
+                else stack.addLast(array[i]);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) sb.append(stack.pollFirst());
+        return sb.toString();
+    }
+}
