bd6c4181cec64030b273e4c3f71ab1221c5c8acc
chenyangcyx <chenyangcyx@outlook.com>
2021-03-09 21:04:04 +0800

    高级算法，No 1-9

--- 高级算法导论/AlgorithmClass/src/Contest1/No1_9.java
+++ 高级算法导论/AlgorithmClass/src/Contest1/No1_9.java
@@ -1,8 +1,10 @@
+package Contest1;
+
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class Main {
+public class No1_9 {
     static class Point {
         private int x = 0;
         private int y = 0;
@@ -34,7 +36,76 @@ public class Main {
         }
     }
 
-    public static double calculateBearingToPoint(double currentBearing, int currentX, int currentY, int targetX, int targetY) {
+    public void No1_9() {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            if (N == 0) {
+                System.out.println(-1);
+                continue;
+            }
+            Point[] allpoint = new Point[N];
+            for (int i = 0; i < N; i++) {
+                int _x = sc.nextInt();
+                int _y = sc.nextInt();
+                Point _point = new Point(_x, _y);
+                allpoint[i] = _point;
+            }
+            PriorityQueue<Point> result = new PriorityQueue<>((o1, o2) -> {
+                if (o1.getX() == o2.getX()) return o1.getY() - o2.getY();
+                else return o1.getX() - o2.getX();
+            });
+            int len = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (i == j) continue;
+                    int[] judge = new int[N];
+                    for (int k = 0; k < N; k++) {
+                        int a = allpoint[j].getY() - allpoint[i].getY();
+                        int b = allpoint[i].getX() - allpoint[j].getX();
+                        int c = allpoint[i].getX() * allpoint[j].getY() - allpoint[i].getY() * allpoint[j].getX();
+                        judge[k] = a * (allpoint[k].getX()) + b * (allpoint[k].getY()) - c;
+                    }
+                    if (judgeArray(judge)) {
+                        result.add(allpoint[i]);
+                        break;
+                    }
+                }
+            }
+            if (result.size() == 0) {
+                System.out.println(-1);
+                continue;
+            }
+            boolean if_first = true;
+            while (!result.isEmpty()) {
+                Point p = result.poll();
+                if (if_first) {
+                    System.out.print(p.getX() + " " + p.getY());
+                    if_first = false;
+                } else System.out.print(", " + p.getX() + " " + p.getY());
+            }
+            System.out.println();
+        }
+    }
+
+    private boolean judgeArray(int[] array) {
+        boolean judge = false;
+        int len1 = 0, len2 = 0;
+        for (int i : array) {
+            if (i >= 0)
+                len1++;
+        }
+        for (int i : array) {
+            if (i <= 0)
+                len2++;
+        }
+        if (len1 == array.length || len2 == array.length)
+            judge = true;
+        return judge;
+    }
+
+    public double calculateBearingToPoint(double currentBearing, int currentX, int currentY, int targetX, int targetY) {
         double angle;
         int x = targetX - currentX;
         int y = targetY - currentY;
@@ -42,7 +113,7 @@ public class Main {
         return angle < 0 ? angle + 360 : angle;
     }
 
-    public static PriorityQueue<Point> findPoint(Point[] points) {
+    public PriorityQueue<Point> findPoint(Point[] points) {
         Point minPoint = null;
         double nowBearing;
         double nextBearing;
@@ -105,12 +176,12 @@ public class Main {
         return result;
     }
 
-    public static void main(String[] args) {
+    public void No1_9_2() {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         while (T-- > 0) {
             int N = sc.nextInt();
-            if (N == 0) {
+            if(N==0){
                 System.out.println(-1);
                 continue;
             }
@@ -122,7 +193,7 @@ public class Main {
                 allpoint[i] = _point;
             }
             PriorityQueue<Point> result = findPoint(allpoint);
-            if (result.size() == 0) {
+            if(result.size()==0){
                 System.out.println(-1);
                 continue;
             }
