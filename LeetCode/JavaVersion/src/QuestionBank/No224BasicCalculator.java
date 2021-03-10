d311ca1b80ddf33f1b2a9276e708404274ad72fb
chenyangcyx <chenyangcyx@outlook.com>
2021-03-10 21:43:59 +0800

    Leetcode224，未通过，时间超时，修改1

--- LeetCode/JavaVersion/src/QuestionBank/No224BasicCalculator.java
+++ LeetCode/JavaVersion/src/QuestionBank/No224BasicCalculator.java
@@ -1,6 +1,5 @@
 package QuestionBank;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class No224BasicCalculator {
@@ -27,22 +26,21 @@ public class No224BasicCalculator {
         // 队列用来存储后缀表达式，栈用来存储操作符
         LinkedList<Node> biaodashi = new LinkedList<>();
         LinkedList<Node> op_stack = new LinkedList<>();
+        // 首部添加一个0，防止负数为首位
+        biaodashi.addLast(new Node(0, ' ', true));
         // 开始遍历整个字符串
-        ArrayList<Character> array = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c != ' ') array.add(c);
-        }
-        for (int i = 0; i < array.size(); ) {
-            char c = array.get(i);
+        char[] array = s.replace(" ", "").replace("(-", "(0-").toCharArray();
+//        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length; ) {
+            char c = array[i];
 //            System.out.println("当前的c："+c);
             // 如果该字符是数字
-            if (c >= '0' && c <= '9') {
+            if (Character.isDigit(c)) {
                 int num = 0;
-                while (i < array.size() && c >= '0' && c <= '9') {
-                    c = array.get(i++);
+                while (i < array.length && Character.isDigit(c)) {
+                    c = array[i++];
                     num = num * 10 + (c - '0');
-                    if (i < array.size()) c = array.get(i);
+                    if (i < array.length) c = array[i];
                 }
 //                System.out.println("num的值："+num);
                 biaodashi.addLast(new Node(num, ' ', true));
@@ -59,10 +57,14 @@ public class No224BasicCalculator {
                 }
                 // +符号，-符号
                 else {
-                    // 将前面的操作符全部插入到后缀表达式中
-                    while (!op_stack.isEmpty() && op_stack.peekLast().op != '(') biaodashi.addLast(op_stack.pollLast());
-                    // 当前的操作符插入
-                    op_stack.addLast(new Node(0, c, false));
+                    if (op_stack.size() == 0) op_stack.addLast(new Node(0, c, false));
+                    else {
+                        // 将前面的操作符全部插入到后缀表达式中
+                        while (!op_stack.isEmpty() && op_stack.peekLast().op != '(')
+                            biaodashi.addLast(op_stack.pollLast());
+                        // 当前的操作符插入
+                        op_stack.addLast(new Node(0, c, false));
+                    }
                 }
                 i++;
             }
@@ -84,25 +86,21 @@ public class No224BasicCalculator {
         // 将栈中剩余的内容全部弹出
         while (!op_stack.isEmpty()) biaodashi.addLast(op_stack.pollLast());
 
-        // 输出最后得到的后缀表达式
+//        // 输出最后得到的后缀表达式
 //        StringBuilder sb=new StringBuilder();
 //        for(Node n:biaodashi){
 //            if(n.if_num) sb.append(n.value);
 //            else sb.append(n.op);
 //        }
+//        System.out.println(sb.toString());
 
         // 开始计算后缀表达式
         for (int i = 0; i < biaodashi.size(); i++) {
             Node n = biaodashi.get(i);
             if (n.if_num) op_stack.addLast(n);
             else {
-                int num1 = 0, num2 = 0;
-                if (op_stack.size() == 1) {
-                    num2 = op_stack.pollLast().value;
-                } else {
-                    num2 = op_stack.pollLast().value;
-                    num1 = op_stack.pollLast().value;
-                }
+                int num2 = op_stack.pollLast().value;
+                int num1 = op_stack.pollLast().value;
                 int result = 0;
                 switch (n.op) {
                     case '+':
