704bb5b896d64c1a586a78fce032f1e11f304028
chenyangcyx <chenyangcyx@outlook.com>
2021-03-11 14:52:02 +0800

    LeetCode227. 基本计算器 II

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,8 +1,11 @@
-import QuestionBank.No47PermutationsII;
+import QuestionBank.No227BasicCalculatorII;
 
 public class Main {
     public static void main(String[] args) {
-        int []nums=new int[]{1,1,2};
-        new No47PermutationsII().permuteUnique(nums);
+        String str1="3+2*2";
+        String str2=" 3/2 ";
+        String str3=" 3+5 / 2 ";
+        String str4="1-1+1";
+        System.out.println(new No227BasicCalculatorII().calculate(str4));
     }
 }
