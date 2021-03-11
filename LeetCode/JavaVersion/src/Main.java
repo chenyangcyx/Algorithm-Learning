2a3bf486aef37ee3ae4158349dc31034857dd00c
chenyangcyx <chenyangcyx@outlook.com>
2021-03-11 15:32:03 +0800

    LeetCode227. 基本计算器 II 使用双栈的普遍解法

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,11 +1,8 @@
-import QuestionBank.No227BasicCalculatorII;
+import QuestionBank.No227BasicCalculatorII_2;
 
 public class Main {
     public static void main(String[] args) {
-        String str1="3+2*2";
-        String str2=" 3/2 ";
-        String str3=" 3+5 / 2 ";
-        String str4="1-1+1";
-        System.out.println(new No227BasicCalculatorII().calculate(str4));
+        String str=" 3/2 ";
+        System.out.println(new No227BasicCalculatorII_2().calculate(str));
     }
 }
