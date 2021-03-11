3e4103eb56fcfd5e4784326e0b285160e32fe53f
chenyangcyx <chenyangcyx@outlook.com>
2021-03-11 17:06:29 +0800

    CS-Note BFS 增加参考代码，修改项目JDK为1.8 LeetCode1091. 二进制矩阵中的最短路径

--- LeetCode/JavaVersion/src/CSNotes/BFS/No1091ShortestPathinBinaryMatrix.java
+++ LeetCode/JavaVersion/src/CSNotes/BFS/No1091ShortestPathinBinaryMatrix.java
@@ -1,6 +1,9 @@
 package CSNotes.BFS;
 
+import javafx.util.Pair;
+
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class No1091ShortestPathinBinaryMatrix {
     int[] dir_x = new int[]{-1, 0, 1, -1, 1, -1, 0, 1};
@@ -43,11 +46,8 @@ public class No1091ShortestPathinBinaryMatrix {
     public int shortestPathBinaryMatrix(int[][] grid) {
         if (grid[0][0] == 1) return -1;
         boolean[][] inque = new boolean[grid.length][grid[0].length];
-        LinkedList<Point> queue = new LinkedList<>() {
-            {
-                add(new Point(0, 0, 1));
-            }
-        };
+        LinkedList<Point> queue = new LinkedList<>();
+        queue.add(new Point(0, 0, 1));
         inque[0][0] = true;
         int ans = Integer.MAX_VALUE;
         // 开始进行BFS操作
@@ -71,5 +71,40 @@ public class No1091ShortestPathinBinaryMatrix {
         if (vis_x < 0 || vis_x >= grid.length) return false;
         if (vis_y < 0 || vis_y >= grid[0].length) return false;
         return grid[vis_x][vis_y] != 1;
+    }
+
+    // CS-Note 参考代码
+    public int shortestPathBinaryMatrix2(int[][] grids) {
+        if (grids == null || grids.length == 0 || grids[0].length == 0) {
+            return -1;
+        }
+        int[][] direction = {{1, -1}, {1, 0}, {1, 1}, {0, -1}, {0, 1}, {-1, -1}, {-1, 0}, {-1, 1}};
+        int m = grids.length, n = grids[0].length;
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair<>(0, 0));
+        int pathLength = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            pathLength++;
+            while (size-- > 0) {
+                Pair<Integer, Integer> cur = queue.poll();
+                int cr = cur.getKey(), cc = cur.getValue();
+                if (grids[cr][cc] == 1) {
+                    continue;
+                }
+                if (cr == m - 1 && cc == n - 1) {
+                    return pathLength;
+                }
+                grids[cr][cc] = 1; // 标记
+                for (int[] d : direction) {
+                    int nr = cr + d[0], nc = cc + d[1];
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                        continue;
+                    }
+                    queue.add(new Pair<>(nr, nc));
+                }
+            }
+        }
+        return -1;
     }
 }
