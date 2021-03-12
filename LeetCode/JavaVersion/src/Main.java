328419cdb4c0d00efb8fc81e4713b7f1b997d353
chenyangcyx <chenyangcyx@outlook.com>
2021-03-13 00:29:36 +0800

    LeetCode331. 验证二叉树的前序序列化

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,8 +1,12 @@
-import CSNotes.Backtracking.No90SubsetsII;
+import QuestionBank.No331VerifyPreorderSerializationofaBinaryTree;
 
 public class Main {
     public static void main(String[] args) {
-        int []nums=new int[]{4,4,4,1,4};
-        System.out.println(new No90SubsetsII().subsetsWithDup(nums));
+        String str1="934##1##2#6##";
+        String str2="1#";
+        String str3="9##1";
+        System.out.println(new No331VerifyPreorderSerializationofaBinaryTree().isValidSerialization1(str1));
+        System.out.println(new No331VerifyPreorderSerializationofaBinaryTree().isValidSerialization2(str2));
+        System.out.println(new No331VerifyPreorderSerializationofaBinaryTree().isValidSerialization3(str3));
     }
 }
