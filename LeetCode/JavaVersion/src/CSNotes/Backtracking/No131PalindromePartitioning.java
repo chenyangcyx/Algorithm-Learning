3b0b3278ba2d123b3575ae5de7f07dbd5854409e
chenyangcyx <chenyangcyx@outlook.com>
2021-03-13 01:28:32 +0800

    CS-Note Backtracking LeetCode131. 分割回文串

---
+++ LeetCode/JavaVersion/src/CSNotes/Backtracking/No131PalindromePartitioning.java
@@ -0,0 +1,82 @@
+package CSNotes.Backtracking;
+
+import java.util.*;
+
+public class No131PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new LinkedList<>();
+        dfs(s, 0, s.length(), result, new LinkedList<>());
+//        dfs(s, result, new LinkedList<>());
+        return result;
+    }
+
+    private void dfs(String s, int start, int end, List<List<String>> result, LinkedList<String> path) {
+        if (start >= end) {
+            result.add(new LinkedList<>(path));
+            return;
+        }
+        for (int i = 1; i <= end - start; i++) {
+            if (isHuiwen(s, start, start + i)) {
+                path.addLast(s.substring(start, start + i));
+//                System.out.println("添加："+s.substring(start,start+i));
+                dfs(s, start + i, end, result, path);
+                path.pollLast();
+            }
+        }
+    }
+
+    private void dfs(String s, List<List<String>> result, LinkedList<String> path) {
+        if (s.length() == 0) {
+            result.add(new LinkedList<>(path));
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            String sub = s.substring(0, i + 1);
+            if (isHuiwen(s, 0, i + 1)) {
+                path.addLast(sub);
+//                System.out.println("添加："+sub);
+                dfs(s.substring(i + 1), result, path);
+                path.pollLast();
+            }
+        }
+    }
+
+    private boolean isHuiwen(String s, int start, int end) {
+        int i = start, j = end - 1;
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) return false;
+        }
+        return true;
+    }
+
+    // CS-Note 参考代码
+    public List<List<String>> partition2(String s) {
+        List<List<String>> partitions = new ArrayList<>();
+        List<String> tempPartition = new ArrayList<>();
+        doPartition(s, partitions, tempPartition);
+        return partitions;
+    }
+
+    private void doPartition(String s, List<List<String>> partitions, List<String> tempPartition) {
+        if (s.length() == 0) {
+            partitions.add(new ArrayList<>(tempPartition));
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (isPalindrome(s, 0, i)) {
+                tempPartition.add(s.substring(0, i + 1));
+                doPartition(s.substring(i + 1), partitions, tempPartition);
+                tempPartition.remove(tempPartition.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int begin, int end) {
+        while (begin < end) {
+            if (s.charAt(begin++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,12 +1,8 @@
-import QuestionBank.No331VerifyPreorderSerializationofaBinaryTree;
+import CSNotes.Backtracking.No131PalindromePartitioning;
 
 public class Main {
     public static void main(String[] args) {
-        String str1="934##1##2#6##";
-        String str2="1#";
-        String str3="9##1";
-        System.out.println(new No331VerifyPreorderSerializationofaBinaryTree().isValidSerialization1(str1));
-        System.out.println(new No331VerifyPreorderSerializationofaBinaryTree().isValidSerialization2(str2));
-        System.out.println(new No331VerifyPreorderSerializationofaBinaryTree().isValidSerialization3(str3));
+        String str="aaba";
+        System.out.println(new No131PalindromePartitioning().partition(str));
     }
 }
