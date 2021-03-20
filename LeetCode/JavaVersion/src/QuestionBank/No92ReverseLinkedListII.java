8163a9569e0be40353ccfed6d46ab34153b08ed0
chenyangcyx <chenyangcyx@outlook.com>
2021-03-20 14:00:54 +0800

    LeetCode92. 反转链表 II

---
+++ LeetCode/JavaVersion/src/QuestionBank/No92ReverseLinkedListII.java
@@ -0,0 +1,123 @@
+package QuestionBank;
+
+public class No92ReverseLinkedListII {
+    //    Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 创建一个虚拟头结点
+        ListNode myhead = new ListNode(-1);
+        myhead.next = head;
+
+        // 找到left节点的前一个节点
+        ListNode left_head = myhead;
+        for (int i = 0; i < left - 1; i++) left_head = left_head.next;
+
+        // 找到right节点的位置
+        ListNode right_head = myhead;
+        for (int i = 0; i < right; i++) right_head = right_head.next;
+
+        // 截断链表
+        ListNode right_head2 = right_head.next, left_head2 = left_head.next;
+        right_head.next = null;
+
+        // 链表反转
+        ListNode pre = null, cur = left_head.next;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        // 接上链表
+        left_head.next = right_head;
+        left_head2.next = right_head2;
+
+        return myhead.next;
+    }
+
+    // LeetCode 参考代码1
+    public ListNode reverseBetween_1(ListNode head, int left, int right) {
+        // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode pre = dummyNode;
+        // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+        // 建议写在 for 循环里，语义清晰
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+
+        // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+
+        // 第 3 步：切断出一个子链表（截取链表）
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+
+        // 注意：切断链接
+        pre.next = null;
+        rightNode.next = null;
+
+        // 第 4 步：同第 206 题，反转链表的子区间
+        reverseLinkedList(leftNode);
+
+        // 第 5 步：接回到原来的链表中
+        pre.next = rightNode;
+        leftNode.next = curr;
+        return dummyNode.next;
+    }
+
+    private void reverseLinkedList(ListNode head) {
+        // 也可以使用递归反转一个链表
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+    }
+
+    // LeetCode 参考代码2
+    public ListNode reverseBetween_2(ListNode head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+}
