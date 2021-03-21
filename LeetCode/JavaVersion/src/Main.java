5d6cc8e37cdcb90b24fca072dd2c1e3ad6dbda42
chenyangcyx <chenyangcyx@outlook.com>
2021-03-21 14:09:37 +0800

    LeetCode150. 逆波兰表达式求值

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,7 +1,8 @@
-import QuestionBank.No115DistinctSubsequences;
+import QuestionBank.No150EvaluateReversePolishNotation;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new No115DistinctSubsequences().numDistinct("babgbag","bag"));
+        String []token=new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        System.out.println(new No150EvaluateReversePolishNotation().evalRPN(token));
     }
 }
