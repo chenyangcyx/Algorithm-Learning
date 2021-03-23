339391eb1a4f98b35add39b19c3ec3af6eff5a50
chenyangcyx <chenyangcyx@outlook.com>
2021-03-24 00:14:32 +0800

    LeetCode146. LRU 缓存机制

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,8 +1,16 @@
-import QuestionBank.No5LongestPalindromicSubstring;
+import QuestionBank.No146LRUCache;
 
 public class Main {
     public static void main(String[] args) {
-        String str="cbbd";
-        System.out.println(new No5LongestPalindromicSubstring().longestPalindrome(str));
+        No146LRUCache cc=new No146LRUCache(2);
+        cc.put(1,1);
+        cc.put(2,2);
+        System.out.println(cc.get(1));
+        cc.put(3,3);
+        System.out.println(cc.get(2));
+        cc.put(4,4);
+        System.out.println(cc.get(1));
+        System.out.println(cc.get(3));
+        System.out.println(cc.get(4));
     }
 }
