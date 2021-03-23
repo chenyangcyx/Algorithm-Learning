c200403bbbc295ae55eb809f612ff8e3626df931
chenyangcyx <chenyangcyx@outlook.com>
2021-03-23 14:05:47 +0800

    LeetCode191. 位1的个数

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,8 +1,7 @@
-import QuestionBank.No150EvaluateReversePolishNotation;
+import QuestionBank.No191Numberof1Bits;
 
 public class Main {
     public static void main(String[] args) {
-        String []token=new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
-        System.out.println(new No150EvaluateReversePolishNotation().evalRPN(token));
+        System.out.println(new No191Numberof1Bits().hammingWeight(1024));
     }
 }
