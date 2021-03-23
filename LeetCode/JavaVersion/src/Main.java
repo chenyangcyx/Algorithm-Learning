6886a702e8a1d1f246108e8e0f1a3f9225c7cbed
chenyangcyx <chenyangcyx@outlook.com>
2021-03-23 22:36:51 +0800

    LeetCode5. 最长回文子串

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,8 +1,8 @@
-import QuestionBank.No3LongestSubstringWithoutRepeatingCharacters;
+import QuestionBank.No5LongestPalindromicSubstring;
 
 public class Main {
     public static void main(String[] args) {
-        String str="dvdf";
-        System.out.println(new No3LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring(str));
+        String str="cbbd";
+        System.out.println(new No5LongestPalindromicSubstring().longestPalindrome(str));
     }
 }
