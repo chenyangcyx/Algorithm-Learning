6e7511f7c9dcc0140fce006814eddf050a3e3c28
chenyangcyx <chenyangcyx@outlook.com>
2021-03-24 20:58:23 +0800

    LeetCode 15. 三数之和

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,15 +1,13 @@
-import QuestionBank.No9PalindromeNumber;
+import QuestionBank.No15_3Sum;
 
 public class Main {
     public static void main(String[] args) {
-        int x1=121;
-        int x2=-121;
-        int x3=10;
-        int x4=-101;
-        No9PalindromeNumber pn=new No9PalindromeNumber();
-        System.out.println(pn.isPalindrome(x1));
-        System.out.println(pn.isPalindrome(x2));
-        System.out.println(pn.isPalindrome(x3));
-        System.out.println(pn.isPalindrome(x4));
+        int []nums1=new int[]{-1,0,1,2,-1,-4};
+        int []nums2=new int[0];
+        int []nums3=new int[]{0};
+        No15_3Sum sum=new No15_3Sum();
+        System.out.println(sum.threeSum2(nums1));
+        System.out.println(sum.threeSum2(nums2));
+        System.out.println(sum.threeSum2(nums3));
     }
 }
