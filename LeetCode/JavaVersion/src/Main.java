7275af85ab58fc5887382f3d80c465492821d4b8
chenyangcyx <chenyangcyx@outlook.com>
2021-03-25 18:17:20 +0800

    LeetCode 215. 数组中的第K个最大元素

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,23 +1,15 @@
-import QuestionBank.No912SortanArray;
-
-import java.util.Arrays;
+import QuestionBank.No215KthLargestElementinanArray;
 
 public class Main {
     public static void main(String[] args) {
-        // 快速排序
-        int []nums1=new int[]{5,1,1,2,0,0};
-        System.out.println(Arrays.toString(new No912SortanArray().sortArray_1_quicksort(nums1)));
-
-        // 堆排序
-        int []nums2=new int[]{5,1,1,2,0,0};
-        System.out.println(Arrays.toString(new No912SortanArray().sortArray2_heapsort(nums2)));
-
-        // 归并排序1
-        int []nums3=new int[]{5,1,1,2,0,0};
-        System.out.println(Arrays.toString(new No912SortanArray().sortArray3_mergesort1(nums3)));
-
-        // 归并排序2
-        int []nums4=new int[]{5,1,1,2,0,0};
-        System.out.println(Arrays.toString(new No912SortanArray().sortArray3_mergesort2(nums4)));
+        int []nums1=new int[]{3,2,1,5,6,4};
+        int []nums2=new int[]{3,2,3,1,2,4,5,5,6};
+        int []nums3=new int[]{-1,-1};
+        System.out.println(new No215KthLargestElementinanArray().findKthLargest1(nums1,2));
+        System.out.println(new No215KthLargestElementinanArray().findKthLargest1(nums2,4));
+        System.out.println(new No215KthLargestElementinanArray().findKthLargest1(nums3,2));
+        System.out.println(new No215KthLargestElementinanArray().findKthLargest2(nums1,2));
+        System.out.println(new No215KthLargestElementinanArray().findKthLargest2(nums2,4));
+        System.out.println(new No215KthLargestElementinanArray().findKthLargest2(nums3,2));
     }
 }
