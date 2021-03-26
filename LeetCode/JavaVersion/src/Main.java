7683f232f333a8c1a638dfb22e5c296d5c6745ba
chenyangcyx <chenyangcyx@outlook.com>
2021-03-26 16:22:43 +0800

    LeetCode 33. 搜索旋转排序数组

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,12 +1,9 @@
-import QuestionBank.No344ReverseString;
-
-import java.util.Arrays;
+import QuestionBank.No33SearchinRotatedSortedArray;
 
 public class Main {
     public static void main(String[] args) {
-        char []s="hello".toCharArray();
-        No344ReverseString rs=new No344ReverseString();
-        rs.reverseString(s);
-        System.out.println(Arrays.toString(s));
+        int []nums1=new int[]{4,5,6,7,0,1,2};
+        No33SearchinRotatedSortedArray srs=new No33SearchinRotatedSortedArray();
+        System.out.println(srs.search(nums1,0));
     }
 }
---
+++ LeetCode/JavaVersion/src/QuestionBank/No33SearchinRotatedSortedArray.java
@@ -0,0 +1,57 @@
+package QuestionBank;
+
+public class No33SearchinRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int N = nums.length;
+        if (N == 0) return -1;
+        if (N == 1) return nums[0] == target ? 0 : -1;
+        int i = 0, j = N - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) return mid;
+            // 如果左半部分是有序数组
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) j = mid - 1;
+                else i = mid + 1;
+            }
+            // 如果右半部分是有序数组
+            else {
+                if (nums[mid] < target && target <= nums[N - 1]) i = mid + 1;
+                else j = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    // LeetCode 参考代码
+    public int search2(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
