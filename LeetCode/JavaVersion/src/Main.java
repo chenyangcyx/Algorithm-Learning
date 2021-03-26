72b98b9d024795f53582fc56f0ea71cf8b489d96
chenyangcyx <chenyangcyx@outlook.com>
2021-03-26 15:24:19 +0800

    LeetCode 344. 反转字符串

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,17 +1,12 @@
-import QuestionBank.No11ContainerWithMostWater;
+import QuestionBank.No344ReverseString;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int []nums1=new int[]{1,8,6,2,5,4,8,3,7};
-        int []nums2=new int[]{1,1};
-        int []nums3=new int[]{4,3,2,1,4};
-        int []nums4=new int[]{1,2,1};
-        int []nums5=new int[]{1,2,4,3};
-        No11ContainerWithMostWater cww=new No11ContainerWithMostWater();
-        System.out.println(cww.maxArea(nums1));
-        System.out.println(cww.maxArea(nums2));
-        System.out.println(cww.maxArea(nums3));
-        System.out.println(cww.maxArea(nums4));
-        System.out.println(cww.maxArea(nums5));
+        char []s="hello".toCharArray();
+        No344ReverseString rs=new No344ReverseString();
+        rs.reverseString(s);
+        System.out.println(Arrays.toString(s));
     }
 }
