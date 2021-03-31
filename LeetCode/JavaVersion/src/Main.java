0963b5175789575fff003d4bae94dd8e7e75fd82
chenyangcyx <chenyangcyx@outlook.com>
2021-03-31 15:37:05 +0800

    LeetCode 74. 搜索二维矩阵

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,18 +1,13 @@
-import QuestionBank.No14LongestCommonPrefix;
-
-import java.util.Arrays;
+import QuestionBank.No74Searcha2DMatrix;
 
 public class Main {
     public static void main(String[] args) {
-        String []strs1=new String[]{"flower","flow","flight"};
-        String []strs2=new String[]{"dog","racecar","car"};
-        No14LongestCommonPrefix lcp=new No14LongestCommonPrefix();
-        System.out.println(lcp.longestCommonPrefix(strs1));
-        System.out.println(lcp.longestCommonPrefix(strs2));
-
-        lcp.longestCommonPrefix2(strs1);
-        lcp.longestCommonPrefix2(strs2);
-        System.out.println(Arrays.toString(strs1));
-        System.out.println(Arrays.toString(strs2));
+        int [][]matrix1=new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int [][]matrix2=new int[][]{{1,1}};
+        int [][]matrix3=new int[][]{{1,3}};
+        No74Searcha2DMatrix sm=new No74Searcha2DMatrix();
+        System.out.println(sm.searchMatrix(matrix1,13));
+        System.out.println(sm.searchMatrix(matrix2,0));
+        System.out.println(sm.searchMatrix(matrix3,3));
     }
 }
---
+++ LeetCode/JavaVersion/src/QuestionBank/No74Searcha2DMatrix.java
@@ -0,0 +1,92 @@
+package QuestionBank;
+
+public class No74Searcha2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        if (m == 1 && n == 1) return matrix[0][0] == target;
+        int left = -1, right = m - 1;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (matrix[mid][0] <= target) left = mid;
+            else right = mid - 1;
+        }
+        if (left < 0) return false;
+        int search_line = left;
+//        System.out.println(search_line);
+        left = 0;
+        right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[search_line][mid] == target) return true;
+            else if (matrix[search_line][mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+}
+
+
+// LeetCode 参考代码1
+// 2次二分查找
+/*
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rowIndex = binarySearchFirstColumn(matrix, target);
+        if (rowIndex < 0) {
+            return false;
+        }
+        return binarySearchRow(matrix[rowIndex], target);
+    }
+
+    public int binarySearchFirstColumn(int[][] matrix, int target) {
+        int low = -1, high = matrix.length - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (matrix[mid][0] <= target) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean binarySearchRow(int[] row, int target) {
+        int low = 0, high = row.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
+*/
+
+// LeetCode 参考代码2
+// 1次二分查找
+/*
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+*/
