70ad16d0ef2c67e5c50a9e846afc855c84eab875
chenyangcyx <chenyangcyx@outlook.com>
2021-04-06 19:04:20 +0800

    LeetCode 26. 删除有序数组中的重复项

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,12 +1,18 @@
-import QuestionBank.No781RabbitsinForest;
+import QuestionBank.No26RemoveDuplicatesfromSortedArray;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        No781RabbitsinForest rf=new No781RabbitsinForest();
-        System.out.println(rf.numRabbits(new int[]{1,1,2}));
-        System.out.println(rf.numRabbits(new int[]{10,10,10}));
-        System.out.println(rf.numRabbits(new int[]{}));
-        System.out.println(rf.numRabbits(new int[]{1,0,1,0,0}));
-        System.out.println(rf.numRabbits(new int[]{0,0,1,1,1}));
+        No26RemoveDuplicatesfromSortedArray rdsa=new No26RemoveDuplicatesfromSortedArray();
+        int []nums1=new int[]{0,1,1,2,2,2,3,3,3,3,4,5,6,8,8,8,8};
+        int []nums2=new int[]{1,1,2};
+        int []nums3=new int[]{0,0,1,1,1,2,2,3,3,4};
+        rdsa.removeDuplicates(nums1);
+        rdsa.removeDuplicates(nums2);
+        rdsa.removeDuplicates(nums3);
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(Arrays.toString(nums2));
+        System.out.println(Arrays.toString(nums3));
     }
 }
