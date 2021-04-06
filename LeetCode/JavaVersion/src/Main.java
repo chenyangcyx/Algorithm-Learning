02105de98acae8c83351b5f7a68ee7a70775283a
chenyangcyx <chenyangcyx@outlook.com>
2021-04-06 17:59:03 +0800

    LeetCode 781. 森林中的兔子

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,8 +1,12 @@
-import Concurrent.生产者消费者;
+import QuestionBank.No781RabbitsinForest;
 
 public class Main {
     public static void main(String[] args) {
-        生产者消费者 sczxfz=new 生产者消费者();
-        sczxfz.execute();
+        No781RabbitsinForest rf=new No781RabbitsinForest();
+        System.out.println(rf.numRabbits(new int[]{1,1,2}));
+        System.out.println(rf.numRabbits(new int[]{10,10,10}));
+        System.out.println(rf.numRabbits(new int[]{}));
+        System.out.println(rf.numRabbits(new int[]{1,0,1,0,0}));
+        System.out.println(rf.numRabbits(new int[]{0,0,1,1,1}));
     }
 }
