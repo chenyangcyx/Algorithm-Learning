9f668fe24478061e6541f118563544d659e0c110
chenyangcyx <chenyangcyx@outlook.com>
2021-04-06 19:04:13 +0800

    LeetCode 80. 删除有序数组中的重复项 II

---
+++ LeetCode/JavaVersion/src/QuestionBank/No80RemoveDuplicatesfromSortedArrayII.java
@@ -0,0 +1,37 @@
+package QuestionBank;
+
+public class No80RemoveDuplicatesfromSortedArrayII {
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        if (length <= 2) return length;
+        int slow = 0, fast = 2;
+        while (fast < length) {
+            if (nums[slow] != nums[fast]) {
+                nums[slow + 2] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow + 2;
+    }
+
+    // LeetCode 题解
+    // 双指针
+    /*
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+    */
+}
