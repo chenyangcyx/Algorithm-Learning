64788d43d10b9454ae6abe062aaeb1239832c885
chenyangcyx <chenyangcyx@outlook.com>
2021-04-08 21:02:03 +0800

    高级算法，No 5-3 解法2

--- 高级算法导论/AlgorithmClass/src/Contest5/No5_3.java
+++ 高级算法导论/AlgorithmClass/src/Contest5/No5_3.java
@@ -34,3 +34,42 @@ public class No5_3 {
         return -1;
     }
 }
+
+
+/*
+// https://raw.githubusercontent.com/njuselw/AdvancedAlgorithm/master/src/FifthExercise/Problem0503.java
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Problem0503 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            int amount = scanner.nextInt();
+            int[] coins = new int[n];
+            for (int j = 0; j < n; j++) {
+                coins[j] = scanner.nextInt();
+            }
+            System.out.println(getMinCoinCnt(coins, amount));
+        }
+    }
+
+    //这不是无限量的背包问题吗
+    //dp[i]表示拿i钱所需的最小钞票数
+    public static int getMinCoinCnt(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) { //显然如果这个钞票的面额超过所需钱数，就无法拿来凑
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] >= amount + 1 ? -1 : dp[amount];
+    }
+}
+ */
