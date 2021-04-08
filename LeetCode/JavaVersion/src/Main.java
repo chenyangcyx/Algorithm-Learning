dbb550787d1e43ee132a537dddfa49e5f63b88f9
chenyangcyx <chenyangcyx@outlook.com>
2021-04-08 15:44:13 +0800

    LeetCode 81. 搜索旋转排序数组 II

--- LeetCode/JavaVersion/src/Main.java
+++ LeetCode/JavaVersion/src/Main.java
@@ -1,18 +1,10 @@
-import QuestionBank.No26RemoveDuplicatesfromSortedArray;
-
-import java.util.Arrays;
+import QuestionBank.No81SearchinRotatedSortedArrayII;
 
 public class Main {
     public static void main(String[] args) {
-        No26RemoveDuplicatesfromSortedArray rdsa=new No26RemoveDuplicatesfromSortedArray();
-        int []nums1=new int[]{0,1,1,2,2,2,3,3,3,3,4,5,6,8,8,8,8};
-        int []nums2=new int[]{1,1,2};
-        int []nums3=new int[]{0,0,1,1,1,2,2,3,3,4};
-        rdsa.removeDuplicates(nums1);
-        rdsa.removeDuplicates(nums2);
-        rdsa.removeDuplicates(nums3);
-        System.out.println(Arrays.toString(nums1));
-        System.out.println(Arrays.toString(nums2));
-        System.out.println(Arrays.toString(nums3));
+        int []nums=new int[]{2,5,6,0,0,1,2};
+        No81SearchinRotatedSortedArrayII srsa=new No81SearchinRotatedSortedArrayII();
+        System.out.println(srsa.search(nums,0));
+        System.out.println(srsa.search(nums,3));
     }
 }
