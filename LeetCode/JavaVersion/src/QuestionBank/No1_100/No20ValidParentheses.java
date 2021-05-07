09ae5ab0a6b1c17c94b27523c4258ad7109109f4
chenyangcyx <chenyangcyx@outlook.com>
2021-05-07 16:37:35 +0800

    LeetCode 20. 有效的括号

--- LeetCode/JavaVersion/src/QuestionBank/No1_100/No20ValidParentheses.java
+++ LeetCode/JavaVersion/src/QuestionBank/No1_100/No20ValidParentheses.java
@@ -27,3 +27,35 @@ public class No20ValidParentheses {
         return stack.isEmpty();
     }
 }
+
+
+// LeetCode 题解
+/*
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<Character>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(ch)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+*/
