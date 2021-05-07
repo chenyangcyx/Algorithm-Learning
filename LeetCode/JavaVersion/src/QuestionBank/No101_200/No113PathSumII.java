a90ef53f425c2a879f3fbf721c72ef9b631eb7af
chenyangcyx <chenyangcyx@outlook.com>
2021-05-07 23:54:48 +0800

    LeetCode 113. 路径总和 II

---
+++ LeetCode/JavaVersion/src/QuestionBank/No101_200/No113PathSumII.java
@@ -0,0 +1,131 @@
+package QuestionBank.No101_200;
+
+import java.util.*;
+
+public class No113PathSumII {
+    //    Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<List<Integer>> result;
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        result = new ArrayList<>();
+        checkPath(root, targetSum, 0, new LinkedList<>());
+        return result;
+    }
+
+    private void checkPath(TreeNode root, int target, int sum, LinkedList<Integer> path) {
+        if (root == null) return;
+        sum += root.val;
+        path.addLast(root.val);
+        if (root.left == null && root.right == null && sum == target) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        if (root.left != null) {
+            checkPath(root.left, target, sum, path);
+            path.pollLast();
+        }
+        if (root.right != null) {
+            checkPath(root.right, target, sum, path);
+            path.pollLast();
+        }
+    }
+}
+
+// LeetCode 代码1，DFS
+/*
+class Solution {
+    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    Deque<Integer> path = new LinkedList<Integer>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        dfs(root, sum);
+        return ret;
+    }
+
+    public void dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return;
+        }
+        path.offerLast(root.val);
+        sum -= root.val;
+        if (root.left == null && root.right == null && sum == 0) {
+            ret.add(new LinkedList<Integer>(path));
+        }
+        dfs(root.left, sum);
+        dfs(root.right, sum);
+        path.pollLast();
+    }
+}
+ */
+
+// LeetCode 代码2，BFS
+/*
+class Solution {
+    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    Map<TreeNode, TreeNode> map = new HashMap<TreeNode, TreeNode>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return ret;
+        }
+
+        Queue<TreeNode> queueNode = new LinkedList<TreeNode>();
+        Queue<Integer> queueSum = new LinkedList<Integer>();
+        queueNode.offer(root);
+        queueSum.offer(0);
+
+        while (!queueNode.isEmpty()) {
+            TreeNode node = queueNode.poll();
+            int rec = queueSum.poll() + node.val;
+
+            if (node.left == null && node.right == null) {
+                if (rec == sum) {
+                    getPath(node);
+                }
+            } else {
+                if (node.left != null) {
+                    map.put(node.left, node);
+                    queueNode.offer(node.left);
+                    queueSum.offer(rec);
+                }
+                if (node.right != null) {
+                    map.put(node.right, node);
+                    queueNode.offer(node.right);
+                    queueSum.offer(rec);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public void getPath(TreeNode node) {
+        List<Integer> temp = new LinkedList<Integer>();
+        while (node != null) {
+            temp.add(node.val);
+            node = map.get(node);
+        }
+        Collections.reverse(temp);
+        ret.add(new LinkedList<Integer>(temp));
+    }
+}
+ */
