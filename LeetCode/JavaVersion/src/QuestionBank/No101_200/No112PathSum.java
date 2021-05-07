e8ec1fee851acce266f59be582b82de3211ad884
chenyangcyx <chenyangcyx@outlook.com>
2021-05-07 22:29:34 +0800

    LeetCode 112. 路径总和

---
+++ LeetCode/JavaVersion/src/QuestionBank/No101_200/No112PathSum.java
@@ -0,0 +1,84 @@
+package QuestionBank.No101_200;
+
+public class No112PathSum {
+    //    Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        return checkPath(root, 0, targetSum);
+    }
+
+    private boolean checkPath(TreeNode root, int sum, int target) {
+        if (root == null) return false;
+        sum += root.val;
+        if (root.left == null && root.right == null) return sum == target;
+        return checkPath(root.left, sum, target) || checkPath(root.right, sum, target);
+    }
+}
+
+// LeetCode 代码1，BFS
+/*
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        Queue<TreeNode> queNode = new LinkedList<TreeNode>();
+        Queue<Integer> queVal = new LinkedList<Integer>();
+        queNode.offer(root);
+        queVal.offer(root.val);
+        while (!queNode.isEmpty()) {
+            TreeNode now = queNode.poll();
+            int temp = queVal.poll();
+            if (now.left == null && now.right == null) {
+                if (temp == sum) {
+                    return true;
+                }
+                continue;
+            }
+            if (now.left != null) {
+                queNode.offer(now.left);
+                queVal.offer(now.left.val + temp);
+            }
+            if (now.right != null) {
+                queNode.offer(now.right);
+                queVal.offer(now.right.val + temp);
+            }
+        }
+        return false;
+    }
+}
+ */
+
+// LeetCode 代码2，DFS
+/*
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return sum == root.val;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
+ */
