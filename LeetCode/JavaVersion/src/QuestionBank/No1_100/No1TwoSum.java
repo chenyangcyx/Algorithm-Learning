3ce70e1b4f81458ab8055dc6b53ac60338834577
chenyangcyx <chenyangcyx@outlook.com>
2021-08-07 21:23:35 +0800

    Update No1TwoSum.java

--- LeetCode/JavaVersion/src/QuestionBank/No1_100/No1TwoSum.java
+++ LeetCode/JavaVersion/src/QuestionBank/No1_100/No1TwoSum.java
@@ -12,4 +12,15 @@ public class No1TwoSum {
         }
         return new int[2];
     }
+
+    public int[] twoSum2(int[] nums, int target) {
+        for(int i=0;i< nums.length-1;i++){
+            for(int j=i+1;j< nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[2];
+    }
 }
