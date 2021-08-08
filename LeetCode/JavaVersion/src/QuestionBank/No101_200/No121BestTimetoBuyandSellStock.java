f0f535d635627d99079545a0bb946bcd5def85b4
chenyangcyx <chenyangcyx@outlook.com>
2021-08-08 21:08:13 +0800

    Update No121BestTimetoBuyandSellStock.java

--- LeetCode/JavaVersion/src/QuestionBank/No101_200/No121BestTimetoBuyandSellStock.java
+++ LeetCode/JavaVersion/src/QuestionBank/No101_200/No121BestTimetoBuyandSellStock.java
@@ -44,4 +44,62 @@ public class No121BestTimetoBuyandSellStock {
         }
         return dp[len - 1];
     }
+
+    // 动态规划1
+    public int maxProfit_1(int[] prices) {
+        int len = prices.length;
+        // 特殊判断
+        if (len < 2) {
+            return 0;
+        }
+        int[][] dp = new int[len][2];
+
+        // dp[i][0] 下标为 i 这天结束的时候，不持股，手上拥有的现金数
+        // dp[i][1] 下标为 i 这天结束的时候，持股，手上拥有的现金数
+
+        // 初始化：不持股显然为 0，持股就需要减去第 1 天（下标为 0）的股价
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        // 从第 2 天开始遍历
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+
+    // 动态规划2：滚动数组
+    public int maxProfit_2(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        int[][] dp = new int[2][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i % 2][0] = Math.max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1] + prices[i]);
+            dp[i % 2][1] = Math.max(dp[(i - 1) % 2][1], -prices[i]);
+        }
+        return dp[(len - 1) & 1][0];
+    }
+
+    // 动态规划3：空间优化
+    public int maxProfit_3(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[0] = Math.max(dp[0], dp[1] + prices[i]);
+            dp[1] = Math.max(dp[1], -prices[i]);
+        }
+        return dp[0];
+    }
 }
