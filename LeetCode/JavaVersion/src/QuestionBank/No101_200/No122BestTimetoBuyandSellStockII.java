9a640f60020c604deb5a52d05fd36343c969fd62
chenyangcyx <chenyangcyx@outlook.com>
2021-08-08 21:55:23 +0800

    Update No122BestTimetoBuyandSellStockII.java

--- LeetCode/JavaVersion/src/QuestionBank/No101_200/No122BestTimetoBuyandSellStockII.java
+++ LeetCode/JavaVersion/src/QuestionBank/No101_200/No122BestTimetoBuyandSellStockII.java
@@ -2,13 +2,13 @@ package QuestionBank.No101_200;
 
 public class No122BestTimetoBuyandSellStockII {
     public int maxProfit(int[] prices) {
-        int length=prices.length;
-        if(length==0 || (length==2&&prices[0]>prices[1])) return 0;
-        int sum=0;
-        int min=prices[0];
-        for(int i=1;i<length;i++){
-            if(prices[i]>min) sum+=prices[i]-min;
-            min=prices[i];
+        int length = prices.length;
+        if (length == 0 || (length == 2 && prices[0] > prices[1])) return 0;
+        int sum = 0;
+        int min = prices[0];
+        for (int i = 1; i < length; i++) {
+            if (prices[i] > min) sum += prices[i] - min;
+            min = prices[i];
         }
         return sum;
     }
@@ -39,4 +39,110 @@ public class No122BestTimetoBuyandSellStockII {
         return ans;
     }
      */
+
+    // 暴力搜索
+    private int res;
+
+    public int maxProfit_1(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+        this.res = 0;
+        dfs(prices, 0, len, 0, res);
+        return this.res;
+    }
+
+    private void dfs(int[] prices, int index, int len, int status, int profit) {
+
+        if (index == len) {
+            this.res = Math.max(this.res, profit);
+            return;
+        }
+
+        dfs(prices, index + 1, len, status, profit);
+
+        if (status == 0) {
+            // 可以尝试转向 1
+            dfs(prices, index + 1, len, 1, profit - prices[index]);
+
+        } else {
+            // 此时 status == 1，可以尝试转向 0
+            dfs(prices, index + 1, len, 0, profit + prices[index]);
+        }
+    }
+
+    // 动态规划1
+    public int maxProfit_2(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        // 0：持有现金
+        // 1：持有股票
+        // 状态转移：0 → 1 → 0 → 1 → 0 → 1 → 0
+        int[][] dp = new int[len][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < len; i++) {
+            // 这两行调换顺序也是可以的
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[len - 1][0];
+    }
+
+    // 动态规划2：状态数组分开
+    public int maxProfit_3(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        // cash：持有现金
+        // hold：持有股票
+        // 状态数组
+        // 状态转移：cash → hold → cash → hold → cash → hold → cash
+        int[] cash = new int[len];
+        int[] hold = new int[len];
+
+        cash[0] = 0;
+        hold[0] = -prices[0];
+
+        for (int i = 1; i < len; i++) {
+            // 这两行调换顺序也是可以的
+            cash[i] = Math.max(cash[i - 1], hold[i - 1] + prices[i]);
+            hold[i] = Math.max(hold[i - 1], cash[i - 1] - prices[i]);
+        }
+        return cash[len - 1];
+    }
+
+    // 动态规划3：优化空间
+    public int maxProfit_4(int[] prices) {
+        int len = prices.length;
+        if (len < 2) {
+            return 0;
+        }
+
+        // cash：持有现金
+        // hold：持有股票
+        // 状态转移：cash → hold → cash → hold → cash → hold → cash
+
+        int cash = 0;
+        int hold = -prices[0];
+
+        int preCash = cash;
+        int preHold = hold;
+        for (int i = 1; i < len; i++) {
+            cash = Math.max(preCash, preHold + prices[i]);
+            hold = Math.max(preHold, preCash - prices[i]);
+
+            preCash = cash;
+            preHold = hold;
+        }
+        return cash;
+    }
 }
