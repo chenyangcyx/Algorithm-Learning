bb217443759117f31df44be20de73358bd4c1219
chenyangcyx <chenyangcyx@outlook.com>
2021-08-12 21:56:43 +0800

    LeetCode 257. 二叉树的所有路径

---
+++ LeetCode/JavaVersion/src/QuestionBank/No201_300/No257BinaryTreePaths.java
@@ -0,0 +1,158 @@
+package QuestionBank.No201_300;
+
+import java.util.*;
+
+public class No257BinaryTreePaths {
+    //    Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+        if (root != null && root.left == null && root.right == null) {
+            result.add(String.valueOf(root.val));
+            return result;
+        }
+        dfs(result, root, new LinkedList<>());
+        return result;
+    }
+
+    void dfs(List<String> result, TreeNode node, LinkedList<Integer> path) {
+        if (node == null) {
+            return;
+        }
+        path.addLast(node.val);
+        if (node.left == null && node.right == null) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < path.size(); i++) {
+                if (i == 0) {
+                    stringBuilder.append(path.get(i));
+                } else {
+                    stringBuilder.append("->").append(path.get(i));
+                }
+            }
+            result.add(stringBuilder.toString());
+        } else {
+            dfs(result, node.left, path);
+            dfs(result, node.right, path);
+        }
+        path.pollLast();
+    }
+}
+
+// LeetCode 题解1：DFS
+class Solution1 {
+    //    Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<String>();
+        constructPaths(root, "", paths);
+        return paths;
+    }
+
+    public void constructPaths(TreeNode root, String path, List<String> paths) {
+        if (root != null) {
+            StringBuffer pathSB = new StringBuffer(path);
+            pathSB.append(Integer.toString(root.val));
+            if (root.left == null && root.right == null) {  // 当前节点是叶子节点
+                paths.add(pathSB.toString());  // 把路径加入到答案中
+            } else {
+                pathSB.append("->");  // 当前节点不是叶子节点，继续递归遍历
+                constructPaths(root.left, pathSB.toString(), paths);
+                constructPaths(root.right, pathSB.toString(), paths);
+            }
+        }
+    }
+}
+
+// LeetCode 题解2：BFS
+class Solution2 {
+    //    Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<String>();
+        if (root == null) {
+            return paths;
+        }
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        Queue<String> pathQueue = new LinkedList<String>();
+
+        nodeQueue.offer(root);
+        pathQueue.offer(Integer.toString(root.val));
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            String path = pathQueue.poll();
+
+            if (node.left == null && node.right == null) {
+                paths.add(path);
+            } else {
+                if (node.left != null) {
+                    nodeQueue.offer(node.left);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.left.val).toString());
+                }
+
+                if (node.right != null) {
+                    nodeQueue.offer(node.right);
+                    pathQueue.offer(new StringBuffer(path).append("->").append(node.right.val).toString());
+                }
+            }
+        }
+        return paths;
+    }
+}
