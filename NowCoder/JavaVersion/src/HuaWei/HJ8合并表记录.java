0f94ceab60d51afa4beef3091e26c22208a989bc
chenyangcyx <chenyangcyx@outlook.com>
2021-09-14 01:07:54 +0800

    HJ8 合并表记录

---
+++ NowCoder/JavaVersion/src/HuaWei/HJ8合并表记录.java
@@ -0,0 +1,23 @@
+package HuaWei;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class HJ8合并表记录 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int N = sc.nextInt();
+            TreeMap<Integer, Integer> map = new TreeMap<>((o1, o2) -> o1 - o2);
+            for (int i = 0; i < N; i++) {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                map.put(n1, map.getOrDefault(n1, 0) + n2);
+            }
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+        }
+    }
+}
