018147e884c4b6b56d7f2cb1d55bb97cba2e1e18
chenyangcyx <chenyangcyx@outlook.com>
2021-09-13 23:27:11 +0800

    HJ12 字符串反转

---
+++ NowCoder/JavaVersion/src/HuaWei/HJ12字符串反转.java
@@ -0,0 +1,20 @@
+package HuaWei;
+
+import java.util.Scanner;
+
+public class HJ12字符串反转 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.nextLine();
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (int i = input.length() - 1; i >= 0; i--) {
+//            stringBuilder.append(input.charAt(i));
+//        }
+//        System.out.println(stringBuilder);
+//    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(new StringBuilder(sc.nextLine()).reverse());
+    }
+}
