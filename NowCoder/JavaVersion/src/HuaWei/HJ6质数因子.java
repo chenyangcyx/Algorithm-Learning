0d67e44645faa03ad320b6165e0a16385c5629ec
chenyangcyx <chenyangcyx@outlook.com>
2021-09-14 00:05:45 +0800

    HJ6 质数因子

---
+++ NowCoder/JavaVersion/src/HuaWei/HJ6质数因子.java
@@ -0,0 +1,17 @@
+package HuaWei;
+
+import java.util.Scanner;
+
+public class HJ6质数因子 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 2; i * i <= num; i++) {
+            while (num % i == 0) {
+                System.out.print(i + " ");
+                num /= i;
+            }
+        }
+        System.out.println(num == 1 ? "" : num + " ");
+    }
+}
