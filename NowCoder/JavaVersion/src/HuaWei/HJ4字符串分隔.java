7ab154fc47c1fa8478c2d376f230eb018dbb6c22
chenyangcyx <chenyangcyx@outlook.com>
2021-09-13 23:38:17 +0800

    HJ4 字符串分隔

---
+++ NowCoder/JavaVersion/src/HuaWei/HJ4字符串分隔.java
@@ -0,0 +1,23 @@
+package HuaWei;
+
+import java.util.Scanner;
+
+public class HJ4字符串分隔 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            for (int start = 0; start < input.length(); start += 8) {
+                outputOneLine(input.substring(start, Math.min(start + 8, input.length())));
+            }
+        }
+    }
+
+    static void outputOneLine(String line) {
+        System.out.print(line);
+        for (int i = 0; i < 8 - line.length(); i++) {
+            System.out.print('0');
+        }
+        System.out.println();
+    }
+}
