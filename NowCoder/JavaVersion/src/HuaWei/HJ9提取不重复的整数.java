2c92a518b0e49e87be00c075dea2abc7a8ba8960
chenyangcyx <chenyangcyx@outlook.com>
2021-09-14 01:19:31 +0800

    HJ9 提取不重复的整数

---
+++ NowCoder/JavaVersion/src/HuaWei/HJ9提取不重复的整数.java
@@ -0,0 +1,22 @@
+package HuaWei;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HJ9提取不重复的整数 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = String.valueOf(sc.nextInt());
+            char[] array = input.toCharArray();
+            HashSet<Character> set = new HashSet<>();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = array.length - 1; i >= 0; i--) {
+                if (set.add(array[i])) {
+                    stringBuilder.append(array[i]);
+                }
+            }
+            System.out.println(stringBuilder);
+        }
+    }
+}
