c318defe58eb4cb0c88db328c94d45d4a10efe29
chenyangcyx <chenyangcyx@outlook.com>
2021-09-14 14:50:40 +0800

    HJ10 字符个数统计

---
+++ NowCoder/JavaVersion/src/HuaWei/HJ10字符个数统计.java
@@ -0,0 +1,16 @@
+package HuaWei;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HJ10字符个数统计 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        HashSet<Character> hashSet = new HashSet<>();
+        for (char ch : input.toCharArray()) {
+            hashSet.add(ch);
+        }
+        System.out.println(hashSet.size());
+    }
+}
