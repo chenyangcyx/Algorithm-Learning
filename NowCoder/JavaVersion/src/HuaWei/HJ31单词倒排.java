e71c46d65e52e82e71f6ceb8832914ad5b4cb4b0
chenyangcyx <chenyangcyx@outlook.com>
2021-09-14 21:09:51 +0800

    HJ31 单词倒排

---
+++ NowCoder/JavaVersion/src/HuaWei/HJ31单词倒排.java
@@ -0,0 +1,23 @@
+package HuaWei;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HJ31单词倒排 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        // 去除无关字符
+        input = input.replaceAll("[^a-zA-Z]", " ");
+        // 以空格分割字符串
+        String[] arr = input.split(" ");
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (!arr[i].equals("")) {
+                list.add(arr[i]);
+            }
+        }
+        System.out.println(String.join(" ", list));
+    }
+}
